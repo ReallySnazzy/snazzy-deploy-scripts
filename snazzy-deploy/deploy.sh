82dea2c9085232f98f89a4aacba839b664287d72
Jonathan Cooper <drowsysaturn@gmail.com>
2025-10-26 18:08:28 -0400

    Do pull before forking for snazzy-deploy

--- snazzy-deploy/deploy.sh
+++ snazzy-deploy/deploy.sh
@@ -1,3 +1,5 @@
 #!/run/current-system/sw/bin/bash
 export PATH="$PATH:/run/current-system/sw/bin/"
+cd /etc/snazzy-deploy
+GIT_SSH_COMMAND='ssh -i /home/jscooper/.ssh/id_ed25519 -o IdentitiesOnly=yes' git pull origin master
 nohup /etc/snazzy-deploy/restart-processes.sh &
